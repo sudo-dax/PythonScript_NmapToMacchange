2f17a2595dd0f0ca4968fb8fa8c0305de3659d85
sudo-dax <dax.scholtz@gmail.com>
2021-01-17 06:20:38 +1000

    Fix 01

--- macch.py
+++ macch.py
@@ -10,7 +10,7 @@ import socket
 subprocess.call('clear', shell=True)
 
 # Get Subnet IP
-IP = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+IP = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("1.1.1.1", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
 IP_arr = IP.split('.')
 IP_arr[-1] = "1"
 Subnet = ".".join(IP_arr) + "/24"
@@ -18,7 +18,7 @@ Subnet = ".".join(IP_arr) + "/24"
 # Start Network Scan
 print('Scannig Network for Devices')
 print(' ')
-os.system("sudo nmap -sP " + Subnet + """ | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "$3;}' | sort >> ips_macs_test.txt""")
+os.system("sudo nmap -sP " + Subnet + """ | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "$3;}' | sort >> ips_macs_py.txt""")
 print('Scan complete! ~~ Output in ips_macs_py.txt')
 
 # Counting Number of connections per Device so far
--- scan.py
+++ scan.py
@@ -3,13 +3,25 @@
 #Library
 import os
 import subprocess
+import socket
 
 # Clear Screen
 subprocess.call('clear', shell=True)
 
+
+
 print('Scannig Network for Devices')
 print(' ')
 
-os.system("""sudo nmap -sP 10.1.1.1/20 | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "$3;}' | sort >> ips_macs_py.txt""")
+# Get Subnet IP
+IP = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("1.1.1.1", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+IP_arr = IP.split('.')
+IP_arr[-1] = "1"
+Subnet = ".".join(IP_arr) + "/24"
+
+# Start Network Scan
+print('Scannig Network for Devices')
+print(' ')
+os.system("sudo nmap -sP " + Subnet + """ | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "$3;}' | sort >> ips_macs_test.txt""")
 
 print('Scan complete! ~~ Output in ips_macs_py.txt')
